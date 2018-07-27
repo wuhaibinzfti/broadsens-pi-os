91cb743ea4631f7fa71880c369ba0f945938f7dc
wuhaibinzfti <haibin.wu@broadsens.com>
2018-07-27 17:43:27 +0800

    common driver

--- lib/broadsens/sensor/ads122.c
+++ lib/broadsens/sensor/ads122.c
@@ -6,74 +6,68 @@
 
 #define ADS122_CHNNEL_NUM 4
 
-static struct iio_device *ads122_dev;
-static int channel_count;
-static struct iio_channel *channels[ADS122_CHNNEL_NUM];
-static struct iio_buffer  *rxbuf = NULL;
+struct ads122_device {
+    int dev_no;
+    struct iio_device *indio_dev;
+    int channel_count;
+    struct iio_channel *channels[ADS122_CHNNEL_NUM];
+    struct iio_buffer  *rxbuf;
+};
 
-static bool channel_has_attr(struct iio_channel *chn, const char *attr)
-{
-	unsigned int i, nb = iio_channel_get_attrs_count(chn);
-	for (i = 0; i < nb; i++)
-		if (!strcmp(attr, iio_channel_get_attr(chn, i)))
-			return true;
-	return false;
-}
+static struct ads122_device ads122_device[ADS122_DEV_NUM];
 
-int ads122_read_channel(int chn, float *buffer, struct timeval *tv)
+int ads122_buffer_minitor(int *channel, float *value, struct timeval *tv)
 {
     char buf[512];
     int i = 0;
     ssize_t nbytes_rx;
     size_t sample_size, bytes;
     const struct iio_data_format *fmt;
-    float val = 0.0;
-
+    uint32_t tmp;
+#if 0
     nbytes_rx = iio_buffer_refill(rxbuf);
     if (nbytes_rx <= 0) {
         return -1;
     }
 
-    for (i = 0; i < channel_count - 1; i++) {
+    for (i = 0; i < channel_count; i++) {
         fmt = iio_channel_get_data_format(channels[i]);
         sample_size = fmt->length / 8;
-        bytes = iio_channel_read(channels[i], rxbuf, buf, sample_size);
-        if (bytes < 0) {
+        bytes = iio_channel_read_raw(channels[i], rxbuf, buf, sample_size);
+        if (sample_size != bytes) {
             return -1;
         }
-
-        if (2 == sample_size) {
-            val = (float)((int16_t *)buf)[0];
-        } else if (4 == sample_size) {
-            val = (float)(((int32_t *)buf)[0]);
+        if (4 == sample_size) {
+            tmp = *((uint32_t *)buf);
+            *channel = (int)(tmp >> 24);
+            if (*channel >= ADS122_CHANNEL_NUM) {
+                continue;
+            }
+            if (ADS122_TEMP == *channel) {
+                *value = (float)(((int32_t)(tmp << 8)) >> 18) / 32;
+            } else {
+                *value = (float)(((int32_t)(tmp << 8)) >> 8) / 4096;
+            }
+        } else if (8 == sample_size) {
+            tv->tv_sec = ((int64_t *)buf)[0] / 1000000000;
+            tv->tv_usec = ((int64_t *)buf)[0] % 1000000000 / 1000;
         }
-
-        if (channel_has_attr(channels[i], "scale")) {
-            iio_channel_attr_read(channels[i], "scale", buf, sizeof(buf));
-            val *= strtof(buf, NULL);
-        }
-        buffer[i] = val;
     }
-
-    /* timestamp */
-    fmt = iio_channel_get_data_format(channels[i]);
-    sample_size = fmt->length / 8;
-    bytes = iio_channel_read(channels[i], rxbuf, buf, sample_size);
-    if (sample_size != bytes) {
-        return -1;
-    }
-    tv->tv_sec = ((int64_t *)buf)[0] / 1000000000;
-    tv->tv_usec = ((int64_t *)buf)[0] % 1000000000 / 1000;
-
+#endif
     return 0;
 }
 
-int ads122_sensor_init(struct iio_context *ctx)
+static int ads122_iio_register(struct iio_context *ctx, struct ads122_device *device)
 {
-    static struct iio_device *dev;
+    int chan = 0;
+    char buf[20];
     const char *name;
+    struct iio_device *dev;
+    struct iio_buffer *rxbuf;
 
-    dev = iio_context_find_device(ctx, "ads122-0");
+    sprintf(buf, "ads122-%d", device->dev_no);
+
+    dev = iio_context_find_device(ctx, buf);
     if (!dev) {
         printf("could not find iio_dev:ads122\n");
         return -1;
@@ -83,30 +77,41 @@ int ads122_sensor_init(struct iio_context *ctx)
         struct iio_channel *chn = iio_device_get_channel(dev, i);
         if (iio_channel_is_scan_element(chn)) {
             name = iio_channel_get_id(chn);
-            if (!strcmp("voltage1-voltage0", name) || !strcmp("voltage3-voltage2", name) ||
-                !strcmp("temp0", name) || !strcmp("timestamp", name)) {
-                if (channel_count < ADS122_CHNNEL_NUM) {
+            if (!strcmp("voltage", name) || !strcmp("timestamp", name)) {
+                if (chan < ADS122_CHNNEL_NUM) {
                     iio_channel_enable(chn);
-                    channels[channel_count] = chn;
-                    channel_count++;
+                    device->channels[chan++] = chn;
+                    printf("%s\n", name);
                 } else {
                     printf("channel space too small!\n");
                 }
             }
         }
     }
-    if (0 == channel_count) {
+
+    if (0 == chan) {
         printf("No scan elements found \n");
         return -1;
     }
 
-    rxbuf = iio_device_create_buffer(dev, 1, false);
-    if (!rxbuf) {
+    device->rxbuf = iio_device_create_buffer(dev, 1, false);
+    if (!device->rxbuf) {
         printf("Could not create rx buffer\n");
     }
 
-    iio_buffer_set_blocking_mode(rxbuf, false);
-    ads122_dev = dev;
+    iio_buffer_set_blocking_mode(device->rxbuf, false);
+
+    device->indio_dev = dev;
+    device->channel_count = chan;
+
+    return 0;
+}
+
+int ads122_sensor_init(enum ads122_dev_no dev_no, struct iio_context *ctx)
+{
+    struct ads122_device *device = &ads122_device[dev_no];
+    device->dev_no = dev_no;
+    ads122_iio_register(ctx, device);
     return 0;
 }
 
--- lib/broadsens/sensor/ads122.h
+++ lib/broadsens/sensor/ads122.h
@@ -1,7 +1,35 @@
 #ifndef __ADS122_H__
 #define __ADS122_H__
 
-int ads122_sensor_init(struct iio_context *ctx);
-int ads122_read_channel(int chn, float *value, struct timeval *tv);
+enum ads122_dev_no {
+    ADS122_DEV_0 = 0,
+    ADS122_DEV_1,
+    ADS122_DEV_NUM,
+};
+
+enum ads122_channels {
+	ADS122_PAIN0_NAIN1  = 0,
+	ADS122_PAIN0_NAIN2,
+	ADS122_PAIN0_NAIN3,
+	ADS122_PAIN1_NAIN0,
+	ADS122_PAIN1_NAIN2,
+	ADS122_PAIN1_NAIN3,
+	ADS122_PAIN2_NAIN3,
+	ADS122_PAIN3_NAIN2,
+	ADS122_PAIN0_NAVSS,
+	ADS122_PAIN1_NAVSS,
+	ADS122_PAIN2_NAVSS,
+	ADS122_PAIN3_NAVSS,
+	ADS122_PREF_NREF_4,
+	ADS122_AVDD_AVSS_4,
+	ADS122_AINP_AVDDVSS,
+	ADS122_PREF_NREF,
+	ADS122_TEMP,
+	ADS122_CHANNEL_NUM,
+};
+
+int ads122_sensor_init(enum ads122_dev_no dev_no, struct iio_context *ctx);
+int ads122_buffer_minitor(int *channel, float *value, struct timeval *tv);
+int ads122_read_channel(int chan, float *data, bool blocking);
 
 #endif  /* BNO055_H */
